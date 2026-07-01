bd081ff1ff21408a4342aa0be120eba234e45676
root <root@ip-172-31-34-92.ec2.internal>
2026-07-01 19:45:34 +0000

    Create Node.js application

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("🚀 CI/CD Pipeline Successfully Deployed!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
